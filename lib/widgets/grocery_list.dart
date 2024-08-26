3ce16097906c0524f61277a0ec2eb1112c51f39b
rhtmahour <rhtmahour@gmail.com>
2024-08-26 17:46:16 +0530

    todays code on date 26-08-2024

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,35 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+        MaterialPageRoute(builder: (ctx) => const NewItem()));
+
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No Items Added Yet.'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(
+              _groceryItems[index].name,
+              style: GoogleFonts.roboto(),
+            ),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+              style: GoogleFonts.montserrat(fontSize: 20),
+            ),
+          ),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(
-            groceryItems[index].name,
-            style: GoogleFonts.roboto(),
-          ),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
-            style: GoogleFonts.montserrat(fontSize: 20),
-          ),
-        ),
-      ),
+      body: content,
     );
   }
 }
